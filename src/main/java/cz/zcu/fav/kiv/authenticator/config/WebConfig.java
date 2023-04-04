eb22e8b44f10e1abf5ea18305ff10a636cea1d1e
Petr Urban <urbanp@gapps.zcu.cz>
2023-04-04 15:35:18 +0200

    new: #10228 Autentikátor microservice desc: přidání cors pravidel (bude nutné to přidat)

---
+++ src/main/java/cz/zcu/fav/kiv/authenticator/config/WebConfig.java
@@ -0,0 +1,21 @@
+package cz.zcu.fav.kiv.authenticator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+}
